17a3895ac1d8e236ecb034e7f885ded8a3bd0673
Davlatbek <125478427+davlatbekzoirov@users.noreply.github.com>
2024-08-13 14:56:07 +0500

    Update reklama.py

--- states/reklama.py
+++ states/reklama.py
@@ -2,9 +2,3 @@ from aiogram.fsm.state import State, StatesGroup
 
 class Adverts(StatesGroup):
     adverts = State()
-
-class BookState(StatesGroup):
-    title = State()
-    price = State()
-    description = State()
-    photo = State()
